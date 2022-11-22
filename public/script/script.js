b5fec8b55b0649d0438d273276385b6f8dec0b10
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2022-11-22 23:15:07 +0800

    new rule

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,11 @@
 let end = document.querySelectorAll('.end');
+let choices = document.querySelectorAll('.choices')
 
 end.forEach((item)=>{
     item.disabled = true;
     item.style.filter = "grayscale(100%)"
+})
+
+choices.forEach((choice)=>{
+    choice.disabled=true
 })
