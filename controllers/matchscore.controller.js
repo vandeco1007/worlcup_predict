05141b8a0b39c5ef02bca4be361b1a2dc1001317
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2022-11-23 22:03:00 +0800

    .

--- controllers/matchscore.controller.js
+++ controllers/matchscore.controller.js
@@ -1,21 +1,25 @@
 const matchscore = require('../models/matchscore.model')
+const option = require('../models/option.model')
 
 module.exports = async(req,res,next)=>{
     let {...body} = req.body
     try {
         let checklist = await matchscore.findOne({ 'playerId': body.playerId })
         if(!checklist){
+            let choice = await option.find()
+            let choiceValue = choice[0]
             let create = await matchscore.create({
                 playerId: body.playerId,
-                result1: "Morocco "+body.team1score+" - "+body.team2score+" Croatia",
-                result2: "Đức : "+body.team3score+" - "+body.team4score+" Nhật Bản",
-                result3: "Tây Ban Nha "+body.team5score+" - "+body.team6score+" Costa Rica",
-                result4: "Bỉ "+body.team7score+" - "+body.team8score+" Canada",
+                result1: choiceValue.team1Name+" "+body.team1score+" - "+body.team2score+" "+choiceValue.team2Name,
+                result2: choiceValue.team3Name+" "+body.team3score+" - "+body.team4score+" "+choiceValue.team4Name,
+                result3: choiceValue.team5Name+" "+body.team5score+" - "+body.team6score+" "+choiceValue.team6Name,
+                result4: choiceValue.team7Name+" "+body.team7score+" - "+body.team8score+" "+choiceValue.team8Name,
             })
             // res.json(create)
             
             res.render("success",{result:create})
         }else{
+
             let result1 = []
             if(body.team1score==undefined){
                 result1[0] = checklist.result1
