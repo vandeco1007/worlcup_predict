be022287b5b6e8d2212c2b72276d1d746a8f3ab0
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2022-11-23 21:30:10 +0800

    .

--- public/script/script.js
+++ public/script/script.js
@@ -1,27 +1,24 @@
 let end = document.querySelectorAll('.end');
-let team = document.querySelectorAll('.team');
 let choices = document.querySelectorAll('.choices')
+let team = document.querySelectorAll('.team')
 let score = document.querySelectorAll('.score')
-
-end.forEach((item)=>{
-    item.disabled = true;
-    item.style.filter = "grayscale(100%)"
-    item.style.color = "gray"
-    
-})
 
 team.forEach((item)=>{
     if(item.getAttribute('state')=='end'){
-        item.disabled = true;
         item.style.filter = "grayscale(100%)"
+    }
+})
+
+score.forEach((items)=>{
+    if(items.getAttribute('state')=='end'){
+        items.disabled=true
     }
 })
 
 choices.forEach((choice)=>{
     choice.disabled=true
-}) 
+})
 
 score.forEach((score)=>{
     score.value=0
 })
-
